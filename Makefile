ecf4978ac52feefd227173b62b7a62d3fc4c3560
shinya <mokoshin0720@gmail.com>
2022-08-16 20:05:53 +0900

    [add]first generate by gpt3

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ init:
 	docker-compose up -d
 
 run:
-	docker exec -it python3 python src/sample.py
+	docker-compose exec python3 python src/sample.py
 
 down:
 	docker-compose down
--- src/sample.py
+++ src/sample.py
@@ -1,5 +1,21 @@
+import os
+import openai
+from dotenv import load_dotenv
+
 def main():
-    print("hello docker")
+    openai.api_key = os.getenv("API_KEY")
+
+    prompt = 'My favorite monster in Dragon Quest is'
+    response = openai.Completion.create(
+        engine='davinci',
+        prompt=prompt,
+        max_tokens=100,
+        temperature=0.5,
+        echo=True
+    )
+
+    print(response)
 
 if __name__ == '__main__':
+    load_dotenv()
     main()
