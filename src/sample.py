dc3f8febc1ee94e75e061471b4b0f82698fded3c
shinya <mokoshin0720@gmail.com>
2022-08-16 20:40:58 +0900

    [add]karakara

--- src/sample.py
+++ src/sample.py
@@ -5,13 +5,21 @@ from dotenv import load_dotenv
 def main():
     openai.api_key = os.getenv("API_KEY")
 
-    prompt = 'My favorite monster in Dragon Quest is'
+    desc = 'The following is a conversation with an AI assistant about Bust of Caracalla in the Museo Nazionale Romano. The assistant is helpful, creative, clever, and very friendly.\n'
+    human1 = 'Human: What is your first impression of this work?\n'
+    ai1 = 'AI: My first impression is that it is very finely and realistically carved.\n'
+    human2 = 'Human:How about his eyebrows?\n'
+    ai2 = 'AI:Eyebrows are drawn together to add intensity to the facial expression. It is not overly ornate and looks more like a military leader than a ruler.\n'
+    human3 = 'Human:'
+    prompt = desc + human1 + ai1 + human2 + ai2 + human3
+
     response = openai.Completion.create(
-        engine='davinci',
+        engine="davinci",
         prompt=prompt,
-        max_tokens=100,
-        temperature=0.5,
-        echo=True
+        temperature=0.7,
+        max_tokens=300,
+        frequency_penalty=0.2,
+        presence_penalty=0,
     )
 
     print(response)
